4214835ad382105a2abcb576b5808c9129e4462e
BryanBattagliese <103158012+BryanBattagliese@users.noreply.github.com>
2025-11-14 23:56:37 -0300

    Update and rename T-SQL12.sql to T-SQL-12.sql

--- T-SQL/T-SQL-12.sql
+++ T-SQL/T-SQL-12.sql
@@ -1,3 +1,10 @@
+/*2. Se requiere realizar una verificación de los precios de los COMBOS, para
+ello se solicita que cree el o los objetos necesarios para realizar una
+operación que actualice que el precio de un producto compuesto
+(COMBO) es el 90% de la suma de los precios de sus componentes por
+las cantidades que los componen. Se debe considerar que un producto
+compuesto puede estar compuesto por otros productos compuestos.*/
+
 /* =========================================================
    1) FUNCION: suma recursiva de precios de componentes
       (soporta productos compuestos dentro de productos compuestos)
